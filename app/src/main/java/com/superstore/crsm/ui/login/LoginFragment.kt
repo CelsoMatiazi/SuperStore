76645e39b29578af295a454c04920646c157d365
CelsoMatiazi <celsomatiazi@gmail.com>
2022-04-11 15:16:53 -0300

    implementando autenticação com email e senha

--- app/src/main/java/com/superstore/crsm/ui/login/LoginFragment.kt
+++ app/src/main/java/com/superstore/crsm/ui/login/LoginFragment.kt
@@ -4,16 +4,40 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.FirebaseAuth
 import com.superstore.crsm.R
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
-
+    private lateinit var auth: FirebaseAuth
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+
+
+
         val signUpBtn : TextView = view.findViewById(R.id.login_bottom_txt2)
+        val loginBtn : MaterialButton = view.findViewById(R.id.login_entrar_btn)
+        val email : TextInputEditText = view.findViewById(R.id.login_email_txt)
+        val password : TextInputEditText = view.findViewById(R.id.login_password_txt)
+
+
+        loginBtn.setOnClickListener {
+            auth.signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener{
+                    if(it.isSuccessful){
+                        Toast.makeText(context, "Login - Sucesso", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(context, "Login - Erro", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
 
         signUpBtn.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
--- app/src/main/java/com/superstore/crsm/ui/login/SignUpFragment.kt
+++ app/src/main/java/com/superstore/crsm/ui/login/SignUpFragment.kt
@@ -5,15 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.firebase.auth.FirebaseAuth
 import com.superstore.crsm.R
 
 class SignUpFragment : Fragment(R.layout.fragment_cadastro) {
 
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+
         val backBtn = view.findViewById<ImageView>(R.id.cadastro_back_btn)
+        val signUpBtn = view.findViewById<Button>(R.id.cadastro_entrar_btn)
+        val email : TextInputEditText = view.findViewById(R.id.cadastro_email_txt)
+        val password : TextInputEditText = view.findViewById(R.id.cadastro_password_txt)
+
+        signUpBtn.setOnClickListener {
+            auth.createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+                .addOnCompleteListener{
+                    if(it.isSuccessful){
+                        Toast.makeText(context, "fez cadastro", Toast.LENGTH_LONG).show()
+                    }else{
+                        Toast.makeText(context, "Cadastro - Erro", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+
+
 
         backBtn.setOnClickListener {
             activity?.onBackPressed()
