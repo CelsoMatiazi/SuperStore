1fd0a889d0f42cba0f81baa53feefa5f533d44ed
CelsoMatiazi <celsomatiazi@gmail.com>
2022-04-12 16:12:08 -0300

    implementando login com google

--- app/src/main/java/com/superstore/crsm/ui/login/LoginFragment.kt
+++ app/src/main/java/com/superstore/crsm/ui/login/LoginFragment.kt
@@ -1,29 +1,48 @@
 package com.superstore.crsm.ui.login
-
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
-import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.superstore.crsm.R
+import com.superstore.crsm.util.GoogleLogInActivityContract
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+
     private lateinit var auth: FirebaseAuth
+
+    private val googleSignInRequest = registerForActivityResult(
+        GoogleLogInActivityContract(),
+        ::onGoogleSignInResult
+    )
+
+
+    private val googleSignInOptions : GoogleSignInOptions
+        get() = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("186025361439-8phc1qfdo2cv961f601f8spuo5ug180f.apps.googleusercontent.com")
+            .requestEmail()
+            .requestProfile()
+            .build()
+
+
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         auth = FirebaseAuth.getInstance()
 
-
-
         val signUpBtn : TextView = view.findViewById(R.id.login_bottom_txt2)
         val loginBtn : MaterialButton = view.findViewById(R.id.login_entrar_btn)
+        val googleBtn : MaterialButton = view.findViewById(R.id.login_google_btn)
         val email : TextInputEditText = view.findViewById(R.id.login_email_txt)
         val password : TextInputEditText = view.findViewById(R.id.login_password_txt)
 
@@ -45,7 +64,39 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                 ?.addToBackStack(null)
                 ?.commit()
         }
+
+        googleBtn.setOnClickListener {
+            googleSignInRequest.launch(googleSignInOptions)
+        }
+
+
     }
+
+
+    private fun onGoogleSignInResult(result: GoogleLogInActivityContract.Result?) {
+        if(result is GoogleLogInActivityContract.Result.Success){
+            val token = result.googleSignInAccount.idToken
+            if (token != null) {
+                loginWithGoogle(token)
+            }
+            Toast.makeText(context, "LoginGoogle -> $token", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun loginWithGoogle(token: String){
+        val credential = GoogleAuthProvider.getCredential(token, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+                task: Task<AuthResult> ->
+                if(task.isSuccessful){
+                    Toast.makeText(context, "Login - Register", Toast.LENGTH_LONG).show()
+                }else{
+                    Toast.makeText(context, "Register - Error", Toast.LENGTH_LONG).show()
+                }
+        }
+
+    }
+
 
 
 }
